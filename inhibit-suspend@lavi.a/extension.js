8d1b4c1fc5a2e9ebd7803d5bdfd62d56823241ac
Lavi .A <laviams@gmail.com>
2012-03-11 03:15:26 +0200

    Info in file.

--- inhibit-suspend@lavi.a/extension.js
+++ inhibit-suspend@lavi.a/extension.js
@@ -14,6 +14,10 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	
+ *	Git: https://github.com/lavi741/gnome-shell-extension-inhibit-suspend
+ *	Launchpad: https://launchpad.net/inhibit-suspend
  */
 const DBus = imports.dbus;
 const Lang = imports.lang;
